a664eee91a16b2190ac9044c81fc2eedbdb9ff78
守夜人 <1229248506@qq.com>
2022-01-01 15:36:47 +0800

    mock

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import { mock, Random } from 'mockjs';
+const data = mock({
+  'list|10': [
+    {
+      'id|+1': 1,
+      name: Random.name(),
+    },
+  ],
+});
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return '你好，nestjs!';
+  getHello() {
+    return {
+      code: 200,
+      data,
+    };
   }
 }
